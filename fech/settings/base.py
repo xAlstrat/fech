bfcf5cbb78aef60abde4da3fed78af5d08421da6
Ednar Echeverría <rodak.ro@gmail.com>
2020-01-28 11:29:17 -0300

    Whitenoise removed

--- fech/settings/base.py
+++ fech/settings/base.py
@@ -71,7 +71,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'wagtail.core.middleware.SiteMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 ]
