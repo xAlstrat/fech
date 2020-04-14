a7d1a714bb5dd78b6ee69736885e7b683f3d4395
Ednar Echeverría <rodak.ro@gmail.com>
2020-04-13 21:31:58 -0400

    Sentry

--- blog/models.py
+++ blog/models.py
@@ -516,7 +516,7 @@ class Sharing(CreateMixin):
     SOCIAL_TWITTER = 'TWITTER'
     publish_at = models.DateTimeField("Fecha de publicación", default=now)
     published = models.BooleanField("Se ha publicado", default=False)
-    description = models.TextField("Descripción", max_length=2048, help_text='Número máximo de carácteres depende de la red social utilizada.', null=True)
+    description = models.TextField("Descripción", max_length=2048, help_text='Número máximo de carácteres depende de la red social utilizada (IG: 2200, Twitter: 280).', null=True)
     channel = models.CharField('Red social', max_length=32, choices=[
         # (SOCIAL_FACEBOOK, 'Facebook'),
         (SOCIAL_INSTAGRAM, 'Instagram'),
--- fech/settings/production.py
+++ fech/settings/production.py
@@ -1,9 +1,21 @@
 from .base import *
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 DEBUG = False
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['localhost', '35.153.184.146']
+
+
+sentry_sdk.init(
+    dsn="https://f49b14bed81740999d19f8d12905910b@o287058.ingest.sentry.io/5199419",
+    integrations=[DjangoIntegration()],
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 try:
     from .local import *
